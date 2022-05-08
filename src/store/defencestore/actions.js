7ab7448e92432d6ef9edaec9880b8a498e7ee0af
Slenzy2 <tobiikupolati33@gmail.com>
2022-05-08 19:22:09 +0100

    New commit for conversation

--- src/store/defencestore/actions.js
+++ src/store/defencestore/actions.js
@@ -1232,3 +1232,26 @@ export function getAllConversation({commit}){
     })
   })
 }
+
+
+export function allDepartmentsWithUsers ({commit}){
+  return new Promise((resolve, reject) => {
+    axios({
+      method: "GET",
+      url: backend + '/user/all',
+      headers: {
+        'Authorization': 'Bearer '+localStorage.getItem('userToken')
+      }
+    })
+    .then((response)=>{
+      if(response.status === 200 || response.status === 201){
+        let data = response.data
+        console.log(data);
+      }
+      resolve()
+    })
+    .catch((error) =>{
+      reject()
+    })
+  })
+}
